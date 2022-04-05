d25fbef6d617b613919d6e3e3ef11d8ee1ac6b72
焱丰 李 <liyanfeng129@gmail.com>
2022-04-05 13:40:07 +0200

    Island model v1

--- src/main/java/it/polimi/Island.java
+++ src/main/java/it/polimi/Island.java
@@ -1,4 +1,63 @@
 package it.polimi;
+import java.util.HashMap;
+import EnumSet;
+
+
 
 public class Island {
+    private enum stuTY;
+    //private HashMap<String, int> students;
+    private int[] students;
+    private Island inext_island;
+    private boolean mother_nature;
+    private int size;
+    private String tower;
+    private enum SType {
+        RED,
+        YELLOW,
+        PINK,
+        BLUE,
+        GREEN
+    }
+
+    const int RED = 0;
+    const int YELLOW = 1;
+    const int PINK = 2;
+    const int BLUE = 3;
+    const int GREEN = 4;
+
+    // Sarebbe fatto fuori
+
+    public Island(Island inext_island) {
+        //this.stuTY = EnumSet.StudentType;
+        this.students = new int[5];
+        this.students[RED] = 0;
+        this.students[YELLOW] = 0;
+        this.students[PINK] = 0;
+        this.students[BLUE] = 0;
+        this.students[GREEN] = 0;
+        this.tower = null;
+        this.inext_island = inext_island;
+        this.mother_nature = false;
+        this.size = 1;
+    }
+    public int[] getStutents(){
+        return this.students;
+    }
+
+    public void addStudent(int color){
+        this.students[color]++;
+    }
+
+    public boolean getMotherNature(){
+        return this.mother_nature;
+    }
+
+    public void setTower(String color){
+        this.tower = color;
+    }
+
+    public String getTower() {
+        return this.tower;
+    }
 }
