2a81f1c3749fc2b46538e77ab5e6839fd95ec022
焱丰 李 <liyanfeng129@gmail.com>
2022-04-05 13:46:59 +0200

    Island model v2 with correction

--- src/main/java/it/polimi/Island.java
+++ src/main/java/it/polimi/Island.java
@@ -1,35 +1,23 @@
 package it.polimi;
-import java.util.HashMap;
-import EnumSet;
 
 
 
 public class Island {
-    private enum stuTY;
-    //private HashMap<String, int> students;
     private int[] students;
     private Island inext_island;
     private boolean mother_nature;
     private int size;
     private String tower;
-    private enum SType {
-        RED,
-        YELLOW,
-        PINK,
-        BLUE,
-        GREEN
-    }
-
     const int RED = 0;
     const int YELLOW = 1;
     const int PINK = 2;
     const int BLUE = 3;
     const int GREEN = 4;
 
-    // Sarebbe fatto fuori
+
 
     public Island(Island inext_island) {
-        //this.stuTY = EnumSet.StudentType;
+
         this.students = new int[5];
         this.students[RED] = 0;
         this.students[YELLOW] = 0;
