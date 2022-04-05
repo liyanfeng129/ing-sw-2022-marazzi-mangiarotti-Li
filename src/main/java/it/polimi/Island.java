b30c7d03cd0da71e1efa82ed738273e0342a0f7e
焱丰 李 <liyanfeng129@gmail.com>
2022-04-05 20:13:20 +0200

    Island model v2 with correction

--- src/main/java/it/polimi/Island.java
+++ src/main/java/it/polimi/Island.java
@@ -3,31 +3,22 @@ package it.polimi;
 
 
 
-public class Island {
 
-    //private HashMap<String, int> students;
-    private int[] students;
+public class Island {
+    private final int[] students;
     private Island inext_island;
     private boolean mother_nature;
     private int size;
     private String tower;
 
-    final static int RED = 0;
-    final static int YELLOW = 1;
-    final static int PINK = 2;
-    final static int BLUE = 3;
-    final static int GREEN = 4;
-
-    // Sarebbe fatto fuori
-
     public Island(Island inext_island) {
-        //this.stuTY = EnumSet.StudentType;
+
         this.students = new int[5];
-        this.students[RED] = 0;
-        this.students[YELLOW] = 0;
-        this.students[PINK] = 0;
-        this.students[BLUE] = 0;
-        this.students[GREEN] = 0;
+        this.students[SType.RED.ordinal()] = 0;
+        this.students[SType.YELLOW.ordinal()] = 0;
+        this.students[SType.PINK.ordinal()] = 0;
+        this.students[SType.BLUE.ordinal()] = 0;
+        this.students[SType.GREEN.ordinal()] = 0;
         this.tower = null;
         this.inext_island = inext_island;
         this.mother_nature = false;
@@ -44,6 +35,8 @@ public class Island {
     public boolean getMotherNature(){
         return this.mother_nature;
     }
+
+    public void setMotherNature(boolean mother_nature){this.mother_nature = mother_nature; }
 
     public void setTower(String color){
         this.tower = color;
---
+++ src/main/java/it/polimi/SType.java
@@ -0,0 +1,9 @@
+package it.polimi;
+
+public enum SType {
+        RED,
+        YELLOW,
+        PINK,
+        BLUE,
+        GREEN;
+}
