757e0a7e67458d88e24d882982e2725ad2e3e8d1
焱丰 李 <liyanfeng129@gmail.com>
2022-04-22 10:52:58 +0200

    client and server can now connect via ip address and exchange simple messages

--- src/main/java/it/polimi/ingsw/server/EriantysServer.java
+++ src/main/java/it/polimi/ingsw/server/EriantysServer.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,6 +15,11 @@ public class EriantysServer {
         {
             //Create socket server
             ServerSocket server = new ServerSocket(12345);
+
+            InetAddress iAddress = InetAddress.getLocalHost();
+            String server_IP = iAddress.getHostAddress();
+            System.out.println("Server IP address : " +server_IP);
+            
 
             // declare socket client
             Socket client;
@@ -37,9 +43,21 @@ public class EriantysServer {
                 message = in.readLine();
                 switch (message)
                 {
-                    case "":
-
+                    case "1":
+                        System.out.println(client+"asking for option 1");
+                        out.println("ok, option 1");
                         break;
+                    case "2":
+                        System.out.println(client+"asking for option 2");
+                        out.println("ok, option 2");
+                        break;
+                    case "try to connect":
+                        System.out.println(client+"trying to connect");
+                        out.println("ok, you are connected");
+                        break;
+
+                    default:
+                        out.println("not ok, no option valid for this");
                 }
                 // close client
                 client.close();
