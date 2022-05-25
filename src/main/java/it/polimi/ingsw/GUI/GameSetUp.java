7d4f51d3d27932dd261c218e3507d3fb14bd3154
leonardomarazzi <leonardo.marazzi@mail.polimi.it>
2022-05-25 14:23:30 +0200

    Added GUI

---
+++ src/main/java/it/polimi/ingsw/GUI/GameSetUp.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameSetUp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass(). getResource("game_set_up.fxml"));
+        // set up stage and scene color
+        stage.setScene( new Scene(root,1500,1000));
+
+        stage.setTitle("Eryantis");
+
+
+
+
+
+        //Image icon = new Image("file:./Users/leonardomarazzi/Desktop/progetto_ing_sftw/wooden_pieces/island2.png");
+
+
+
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/GUI/LoadGame.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.GUI;
+
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
+
+
+public class LoadGame extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass(). getResource("load_game.fxml"));
+        // set up stage and scene color
+        stage.setScene( new Scene(root,1500,1000));
+
+        stage.setTitle("Eryantis");
+
+
+
+
+
+        //Image icon = new Image("file:./Users/leonardomarazzi/Desktop/progetto_ing_sftw/wooden_pieces/island2.png");
+
+
+
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
