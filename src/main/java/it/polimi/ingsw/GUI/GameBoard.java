1a3bacbe1e18a7f20665450a5ff4047ce12cec2e
leonardomarazzi <leonardo.marazzi@mail.polimi.it>
2022-06-01 10:29:45 +0200

    GUI GameBoard

--- src/main/java/it/polimi/ingsw/GUI/GameBoard.java
+++ src/main/java/it/polimi/ingsw/GUI/GameBoard.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -12,19 +15,19 @@ public class GameBoard extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+
+
         BorderPane root = new BorderPane();
-        Button button = new Button();
-        button.setText("prova");
-        root.setCenter(button);
-        Scene scene = new Scene (root, Color.LIGHTSKYBLUE);
-        stage.setTitle("Eryantis");
-        stage.setScene(scene);
+        Scene scene = new Scene (root);
         stage.setFullScreen(true);
 
+        stage.setTitle("Eryantis");
+        stage.setScene(scene);
 
-
-
-
+        ImageView img_1 = new ImageView(new Image(getClass().getResourceAsStream("File: island1.png")));
+        Button button = new Button("prova",img_1);
+        root.setCenter(button);
 
         stage.show();
 
