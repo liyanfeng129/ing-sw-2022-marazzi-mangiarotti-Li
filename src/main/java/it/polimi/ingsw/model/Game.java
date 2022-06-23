8ef1a9beb55917cac48fb866306b20994ce1e8f9
leonardomarazzi <leonardo.marazzi@mail.polimi.it>
2022-06-23 17:31:46 +0200

    caracte 9 and 11 now have the ability to get user data

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -114,7 +114,7 @@ public class Game implements Serializable {
                     /** TODO
                      * I character devono essere genereali
                      */
-                    characterCards.add(new Character5());
+                    characterCards.add(new Character9());
                     characterCards.add(new Character1(table.getBag().draw(4)));
                     characterCards.add(new Character11(table.getBag().draw(4)));
 
