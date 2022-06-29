6100c8382a9be942c99ebf2b731ff9c7cab5669f
leonardomarazzi <leonardo.marazzi@mail.polimi.it>
2022-06-29 19:23:03 +0200

    added endame scene

---
+++ src/main/java/it/polimi/ingsw/GUI/EndGameController.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EndGameController {
+
+    @FXML
+    Label label;
+
+    @FXML
+    public void back(){
+
+
+    }
+
+    public void setLabel(){
+
+    }
+
+
+    }
