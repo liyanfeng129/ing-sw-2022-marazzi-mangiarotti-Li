b88297ec74c93473a764b3ebb3a92df11c84c7e1
焱丰 李 <liyanfeng129@gmail.com>
2022-07-01 17:21:31 +0200

    get ip

---
+++ src/main/java/it/polimi/ingsw/client/GetMyIPAddress.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.client;
+
+//This program is find your exact LAN(Local Machine on which your are       //runing this program) IP Address
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class GetMyIPAddress {
+    public static void main(String gks[]) throws SocketException{
+
+        System.out.println(metodo());
+
+    }
+    public static String metodo()
+    {
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface iface = interfaces.nextElement();
+                if (iface.isLoopback() || !iface.isUp() || iface.isVirtual() || iface.isPointToPoint())
+                    continue;
+
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                while(addresses.hasMoreElements()) {
+                    InetAddress addr = addresses.nextElement();
+
+                    final String ip = addr.getHostAddress();
+                    if(Inet4Address.class == addr.getClass()) return ip;
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+}
